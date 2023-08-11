c0d7545da2066a5acb7b2f2dc708b6e12c07dedc
Johan De Wit <johan.dewit@sfpd.fgov.be>
2023-08-11 14:54:58 +0200

    [TEMP] debug

--- lib/puppet/provider/mongodb_user/mongodb.rb
+++ lib/puppet/provider/mongodb_user/mongodb.rb
@@ -78,6 +78,7 @@ Puppet::Type.type(:mongodb_user).provide(:mongodb, parent: Puppet::Provider::Mon
         end
       end
 
+      Puppet.debug("XXXXXXXXXX about to create user with command: #{command}")
       mongo_eval("db.runCommand(#{command.to_json})", @resource[:database])
     else
       Puppet.warning 'User creation is available only from master host'
